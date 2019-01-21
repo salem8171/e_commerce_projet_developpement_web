abc809c40b63da7e7846430dc5ce95a353f3c070
Med Salem Krichen <mohamed-salem.krichen@tutanota.com>
2019-01-21 13:12:43 +0100

    More cleaning up in the client area

--- customer_register.php
+++ customer_register.php
@@ -1,108 +1,35 @@
 <?php include("includes/db.php"); ?>
-<!DOCTYPE html>
-<?php 
-session_start();
-include("functions/functions.php");
-?>
-<html lang="en">
-	<head>
-		<meta charset="utf-8">
-		<title>Bootstrap E-commerce Templates</title>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
-		<!--[if ie]><meta content='IE=8' http-equiv='X-UA-Compatible'/><![endif]-->
-		<!-- bootstrap -->
-		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">      
-		<link href="bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">		
-		<link href="themes/css/bootstrappage.css" rel="stylesheet"/>
-		
-		<!-- global styles -->
-		<link href="themes/css/flexslider.css" rel="stylesheet"/>
-		<link href="themes/css/main.css" rel="stylesheet"/>
+<?php include("includes/header.php"); ?>
 
-		<!-- scripts -->
-		<script src="themes/js/jquery-1.7.2.min.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>				
-		<script src="themes/js/superfish.js"></script>	
-		<script src="themes/js/jquery.scrolltotop.js"></script>
-		<!--[if lt IE 9]>			
-			<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-			<script src="js/respond.min.js"></script>
-		<![endif]-->
-	</head>
-    <body>		
-			<div id="top-bar" class="container">
-			<div class="row">
-					
-					<div class="span3">
-			      <form method="get" action="results.php" enctype="multipart/form-data">
-			 	<input type="text" class="input-block-level search-query" name="user_query" placeholder="Search a Product"/ > 
-				</div>
-				<div class="span1">	     <input type="submit" name="search" value="" >			</div>	
-			</form>
-				<div class="span8">
-					<div class="account pull-right">
-						<ul class="user-menu">				
-							<li><a href="#">Home</a></li>
-							<li><a href="cart.php">Your Cart</a></li>
-							<li><a href="checkout.php">Checkout</a></li>					
-							<li><a href="customer_register.php">Login</a></li>		
-						</ul>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="wrapper" class="container">
-			<section class="navbar main-menu">
-				<div class="navbar-inner main-menu">				
-					<a href="index.php" class="logo pull-left"><img src="themes/images/logo.png" class="site_logo" alt=""></a>
-					<nav id="menu" class="pull-right">
-						<ul>
-							
-						</ul>
-					</nav>
-				</div>
-			</section>
-					
-			<section class="header_text sub">
-			<img class="pageBanner" src="themes/images/pageBanner.png" alt="New products" >
-			
-			</section>			
-			<section class="main-content">				
-				<div class="row">
-			
-					<div class="span7">					
-						<h4 class="title"><span class="text"><strong>Register</strong> Form</span></h4>
-				<form action="customer_register.php" method="post" enctype="multipart/form-data">
-					
-					<table align="center" width="750">
-						
-					
-						<tr>
-							<td align="right">Customer Name:</td>
-							<td><input type="text" name="c_name" required/></td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer Email:</td>
-							<td><input type="text" name="c_email" required/></td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer Password:</td>
-							<td><input type="password" name="c_pass" required/></td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer Image:</td>
-							<td><input type="file" name="c_image" required/></td>
-						</tr>
-						
-						
-						
-						<tr>
-							<td align="right">Customer Country:</td>
-							<td>
+<section class="header_text sub">
+<img class="pageBanner" src="themes/images/pageBanner.png" alt="New products" >
+
+</section>			
+<section class="main-content">				
+	<div class="row">
+		<div class="span7">					
+			<h4 class="title"><span class="text"><strong>Register</strong> Form</span></h4>
+			<form action="customer_register.php" method="post" enctype="multipart/form-data">		
+				<table align="center" width="750">
+					<tr>
+						<td align="right">Customer Name:</td>
+						<td><input type="text" name="c_name" required/></td>
+					</tr>			
+					<tr>
+						<td align="right">Customer Email:</td>
+						<td><input type="text" name="c_email" required/></td>
+					</tr>			
+					<tr>
+						<td align="right">Customer Password:</td>
+						<td><input type="password" name="c_pass" required/></td>
+					</tr>			
+					<tr>
+						<td align="right">Customer Image:</td>
+						<td><input type="file" name="c_image" required/></td>
+					</tr>
+					<tr>
+						<td align="right">Customer Country:</td>
+						<td>
 							<select name="c_country">
 								<option>Select a Country</option>
 								<option>Afghanistan</option>
@@ -114,50 +41,35 @@ include("functions/functions.php");
 								<option>United Arab Emirates</option>
 								<option>United States</option>
 								<option>United Kingdom</option>
-							</select>
-							
-							</td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer City:</td>
-							<td><input type="text" name="c_city" required/></td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer Contact:</td>
-							<td><input type="text" name="c_contact" required/></td>
-						</tr>
-						
-						<tr>
-							<td align="right">Customer Address</td>
-							<td><input type="text" name="c_address" required/></td>
-						</tr>
-						
-						
+							</select>				
+						</td>
+					</tr>			
+					<tr>
+						<td align="right">Customer City:</td>
+						<td><input type="text" name="c_city" required/></td>
+					</tr>			
+					<tr>
+						<td align="right">Customer Contact:</td>
+						<td><input type="text" name="c_contact" required/></td>
+					</tr>			
+					<tr>
+						<td align="right">Customer Address</td>
+						<td><input type="text" name="c_address" required/></td>
+					</tr>
 					<tr align="center">
 						<td colspan="6"><input type="submit" name="register" value="Create Account" /></td>
 					</tr>
-					
-					
-					
-					</table>
-				
-				</form>
-			
-			</div>
+				</table>
+			</form>
+
 		</div>
-		<!--Content wrapper ends-->
-		
-		
-		
-
-
+	</div>
+</section>
+<!--Content wrapper ends-->
 
 <?php 
-	if(isset($_POST['register'])){
-	
-		
+	if(isset($_POST['register']))
+	{
 		$ip = getIp();
 		
 		$c_name = $_POST['c_name'];
@@ -171,103 +83,25 @@ include("functions/functions.php");
 		$c_address = $_POST['c_address'];
 	
 		
-		move_uploaded_file($c_image_tmp,"customer/customer_images/$c_image");
+		move_uploaded_file($c_image_tmp,"customer/customer_images/$c_image");		
+		$insert_c = "insert into customers (customer_ip,customer_name,customer_email,customer_pass,customer_country,customer_city,customer_contact,customer_address,customer_image) values ('$ip','$c_name','$c_email','$c_pass','$c_country','$c_city','$c_contact','$c_address','$c_image')";	
+		$run_c = mysqli_query($con, $insert_c); 		
+		$sel_cart = "select * from cart where ip_add='$ip'";		
+		$run_cart = mysqli_query($con, $sel_cart); 		
+		$check_cart = mysqli_num_rows($run_cart); 		
 		
-		 $insert_c = "insert into customers (customer_ip,customer_name,customer_email,customer_pass,customer_country,customer_city,customer_contact,customer_address,customer_image) values ('$ip','$c_name','$c_email','$c_pass','$c_country','$c_city','$c_contact','$c_address','$c_image')";
-	
-		$run_c = mysqli_query($con, $insert_c); 
-		
-		$sel_cart = "select * from cart where ip_add='$ip'";
-		
-		$run_cart = mysqli_query($con, $sel_cart); 
-		
-		$check_cart = mysqli_num_rows($run_cart); 
-		
-		if($check_cart==0){
-		
-		$_SESSION['customer_email']=$c_email; 
-		
-		echo "<script>alert('Account has been created successfully, Thanks!')</script>";
-		echo "<script>window.open('customer/my_account.php','_self')</script>";
-		
+		if($check_cart==0)
+		{
+			$_SESSION['customer_email']=$c_email; 		
+			echo "<script>alert('Account has been created successfully, Thanks!')</script>";
+			echo "<script>window.open('customer/my_account.php','_self')</script>";
 		}
-		else {
-		
-		$_SESSION['customer_email']=$c_email; 
-		
-		echo "<script>alert('Account has been created successfully, Thanks!')</script>";
-		
-		echo "<script>window.open('checkout.php','_self')</script>";
-		
-		
+		else 
+		{		
+			$_SESSION['customer_email']=$c_email; 			
+			echo "<script>alert('Account has been created successfully, Thanks!')</script>";			
+			echo "<script>window.open('checkout.php','_self')</script>";
 		}
 	}
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-
-
-
-
-		
-			
-			
-			<section id="footer-bar">
-				<div class="row">
-					<div class="span3">
-						<h4>Navigation</h4>
-						<ul class="nav">
-							<li><a href="./index.html">Homepage</a></li>  
-							<li><a href="./about.html">About Us</a></li>
-							<li><a href="./contact.html">Contac Us</a></li>
-							<li><a href="./cart.html">Your Cart</a></li>
-							<li><a href="./register.html">Login</a></li>							
-						</ul>					
-					</div>
-					<div class="span4">
-						<h4>My Account</h4>
-						<ul class="nav">
-							<li><a href="#">My Account</a></li>
-							<li><a href="#">Order History</a></li>
-							<li><a href="#">Wish List</a></li>
-							<li><a href="#">Newsletter</a></li>
-						</ul>
-					</div>
-					<div class="span5">
-						<p class="logo"><img src="themes/images/logo.png" class="site_logo" alt=""></p>
-						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. the  Lorem Ipsum has been the industry's standard dummy text ever since the you.</p>
-						<br/>
-						<span class="social_icons">
-							<a class="facebook" href="#">Facebook</a>
-							<a class="twitter" href="#">Twitter</a>
-							<a class="skype" href="#">Skype</a>
-							<a class="vimeo" href="#">Vimeo</a>
-						</span>
-					</div>					
-				</div>	
-			</section>
-			
-		
-		<script src="themes/js/common.js"></script>
-		<script>
-			$(document).ready(function() {
-				$('#checkout').click(function (e) {
-					document.location.href = "checkout.html";
-				})
-			});
-		</script>		
-    </body>
-</html>
-
-	
-			
+<?php include("includes/footer.php"); ?>
